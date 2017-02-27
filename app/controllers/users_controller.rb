f44f239400b1f1f0bc212f8349d50ae968c7e913
DunlapN <DunlapN@users.noreply.github.com>
2017-02-26 23:08:14 -0600

    added my_likes route and functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def like_index
+    @photos = current_user.liked_photos
+  end
+
   def show
     @user = User.find(params[:id])
   end
