bb6c3e3d57928f099ef30f10c6a07438f9b21356
DunlapN <DunlapN@users.noreply.github.com>
2017-02-27 00:57:47 -0600

    added comments to index

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
  has_many :likes, :class_name => "Like", :foreign_key => "photo_id"
  has_many :users, :through => :likes
  has_many :fans, :through => :likes, :source => :user
-
+ has_many :comments
  validates :user_id, :presence => true
 # validates :caption, :presence => true
  # validates :image, :presence => true
